d9f43e3e4c24da2457fdcaa48300f345a2719366
Samuli Paronen <samuli.paronen@gmail.com>
2020-06-29 16:58:04 +0300

    Enhanced the 'list instances' function

--- shotty/shotty.py
+++ shotty/shotty.py
@@ -1,8 +1,16 @@
 import boto3
+import click
+
+session = boto3.Session(profile_name='shotty')
+ec2 = session.resource('ec2')
+
+@click.command()
+def list_instances():
+    "List EC2 instances"
+    for i in ec2.instances.all():
+            print("ID: " + i.id + " | TYPE: " + i.instance_type + \
+            " | AZ: " + i.placement['AvailabilityZone'] + " | STATE: " + i.state['Name'] + \
+            " | PBL DNS: " + i.public_dns_name)
 
 if __name__ == '__main__':
-    session = boto3.Session(profile_name='shotty')
-    ec2 = session.resource('ec2')
-
-    for i in ec2.instances.all():
-        print(i)
+    list_instances()
